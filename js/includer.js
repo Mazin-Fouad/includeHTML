cd846fd2b0eb238f7f5b565ecc5ae88f3009b7d4
Mazin Fouad <m.fouad@live.de>
2024-09-18 14:51:17 +0200

    Update loadContent function to use sharedComponents directory for HTML files

--- js/includer.js
+++ js/includer.js
@@ -13,6 +13,6 @@ function loadContent(path, elementId) {
     .catch((error) => console.error(`Error loading ${path}: ${error.message}`));
 }
 
-loadContent('htmlPages/navbar.html', 'navbar');
-loadContent('htmlPages/hero.html', 'hero');
-loadContent('htmlPages/footer.html', 'footer');
+loadContent('sharedComponents/navbar.html', 'navbar');
+loadContent('sharedComponents/hero.html', 'hero');
+loadContent('sharedComponents/footer.html', 'footer');
